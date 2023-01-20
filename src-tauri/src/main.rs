8f169672dbd5fc13e55d538e6e9b9ded148694b0
Liam Tietjens <l.tietjens@hotmail.com>
2023-01-20 17:31:45 +1300

    new file:   .gitignore 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   src-tauri/.gitignore 	new file:   src-tauri/Cargo.lock 	new file:   src-tauri/Cargo.toml 	new file:   src-tauri/build.rs 	new file:   src-tauri/icons/128x128.png 	new file:   src-tauri/icons/128x128@2x.png 	new file:   src-tauri/icons/32x32.png 	new file:   src-tauri/icons/Square107x107Logo.png 	new file:   src-tauri/icons/Square142x142Logo.png 	new file:   src-tauri/icons/Square150x150Logo.png 	new file:   src-tauri/icons/Square284x284Logo.png 	new file:   src-tauri/icons/Square30x30Logo.png 	new file:   src-tauri/icons/Square310x310Logo.png 	new file:   src-tauri/icons/Square44x44Logo.png 	new file:   src-tauri/icons/Square71x71Logo.png 	new file:   src-tauri/icons/Square89x89Logo.png 	new file:   src-tauri/icons/StoreLogo.png 	new file:   src-tauri/icons/icon.icns 	new file:   src-tauri/icons/icon.ico 	new file:   src-tauri/icons/icon.png 	new file:   src-tauri/src/main.rs 	new file:   src-tauri/tauri.conf.json 	new file:   src/assets/javascript.svg 	new file:   src/assets/tauri.svg 	new file:   src/index.html 	new file:   src/main.js 	new file:   src/style.css

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,17 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust! BY a monkie with a shit hanging out and his name is {} its you isnt it", name, name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
